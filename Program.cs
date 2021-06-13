f61f4948029e9c4ac8b9a09686060f9ee8f46da9
Borwonlak Phothong <78490883+Mon-Hong@users.noreply.github.com>
2021-06-13 17:26:22 +0700

    ข้อที่2

--- Program.cs
+++ Program.cs
@@ -1,80 +1,146 @@
 ﻿using System;
 
-namespace _5
+namespace _2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Input number of items : ");
-            int number = int.Parse(Console.ReadLine());
-            string[,] name = new string[number, 2];
-
-            for (int i = 0; i < number; i++)
-            {
-
-                Console.Write("Input item name : ");
-
-                name[i, 0] = Console.ReadLine();
-
-                Console.Write("Input item type : ");
-
-                name[i, 1] = Console.ReadLine();
-            }
-            string search;
-            string end;
-
-            do
-            {
-                end = "End";
-                Console.Write("\nSearch : ");
-                search = Console.ReadLine();
-                Console.Write("\n");
-
-                for (int i = 0; i < number; i++)
-                { }
-
-                    if (search == "ShowAll")
-                {
-                    for (int i = 0; i < number; i++)
-                    {
-                        for (int j = 0; j < 2; j++)
-                        {
-                            Console.WriteLine(" {0} ", name[i, j]);
-
-                        }
-                        Console.WriteLine();
-
-                    }
-                }
-
-                for (int i = 0; i < number; i++)
-                {
-                    if (search == name[i, 0])
-                    {
-                        for (int j = 0; j < number; j++)
-                        {
-
-                            Console.WriteLine(" \n{0} ", name[j, 0]);
-
-                        }
-                        
-                        
-                    }
-                }
-                for (int i = 0; i < number; i++)
-                {
-                    if (search == "ShowAll" || search == name[i, 0] )
-                    {
-                        
-                        end = "0";
-                    }
-                }
             
-            } while(end == "0");
+            Console.Write("Number of numbers to sort : ");
+                int num_data = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nEnd");
+                int[] number = new int[num_data];
 
+                for(int i = 0; i < num_data; i++)
+                {
+                
+                    Console.Write("Input number : ") ;
+                
+                    number[i] = int.Parse(Console.ReadLine());  
+                }
+
+            sortone(number, num_data);
+            Console.WriteLine();
+            sorttwo(number, num_data);
+            Console.WriteLine();
+
+            sortthree(number, num_data);
+            sortfour(number, num_data);
+
+
+        }
+
+        static void sortone(int[] number,int num_data)
+        {
+            /*
+            for (int i = 0; i < num_data; i++)
+            {
+                Console.WriteLine(number[i]);   
+            }*/
+
+            
+
+            //วิธีทื่ 1
+            for (int i = 0; i < num_data; i++)
+            {
+
+                for (int j = i+1; j < num_data; j++)
+                { 
+                    
+                    if (number[j] < number[i])
+                    {
+                        int x = number[i];
+
+                        number[i] = number[j];
+
+                        number[j] = x;
+                    }
+                    
+                }
+            }
+            Console.Write("Method 1 ");
+            for (int j = 0; j < num_data; j++)
+            {
+                Console.Write(" {0}",number[j]);
+            }
+        }
+
+
+        static void sorttwo(int[] number, int num_data)
+        {
+
+            //วิธีทื่ 2
+            for (int i = 0; i < num_data-1; i++)
+            {
+
+                for (int j = i + 1; j < num_data; j++)
+                {
+
+                    if (number[j] < number[i])
+                    {
+                        int x = number[i];
+
+                        number[i] = number[j+1];
+
+                        number[j] = x;
+                    }
+
+                }
+            }
+
+
+
+
+
+
+            Console.Write("Method 2 ");
+            for (int j = 0; j < num_data; j++)
+            {
+                Console.Write(" {0}", number[j]);
+            }
+        }
+
+
+        static void sortthree(int[] number, int num_data)
+        {
+
+            //วิธีทื่ 3
+            for (int i = 0; i < num_data - 1; i++)
+            {
+
+                for (int j = i + 1; j < num_data-1; j++)
+                {
+
+                    if (number[j] < number[i])
+                    {
+                        int x = number[i+1];
+
+                        number[i] = number[j + 1];
+
+                        number[j] = x;
+                    }
+
+                }
+            }
+
+
+
+
+
+
+            Console.Write("Method 3 ");
+            for (int j = 0; j < num_data; j++)
+            {
+                Console.Write(" {0}", number[j]);
+            }
+        }
+
+
+
+        static void sortfour(int[] number, int num_data)
+        {
+            
         }
     }
 }
